79eff0bdb7e83484b2ff3945635c47276487166e
valera2872 <barbarov28@protonmail.com>
2026-08-02 09:26:20 +0300

    v0.3.1: понятный осмотр номера и камера
    Убирает двойную прокрутку и делает две ключевые интерактивные сцены понятными без дополнительных объяснений.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import PremiumApp from './PremiumApp';
 import './premium.css';
+import './premium-fix.css';
+import './premiumEnhancements';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
