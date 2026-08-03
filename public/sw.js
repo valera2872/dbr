f22ea590529eebe390a576e3012adb2d1260c2aa
valera2872 <barbarov28@protonmail.com>
2026-08-03 20:22:53 +0300

    Rotate PWA cache for living suspect release

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dbr-v0-6-2-performance-interrogation';
+const CACHE_NAME = 'dbr-v0-6-3-living-suspect';
 const APP_SHELL = ['/dbr/'];
 
 self.addEventListener('install', (event) => {
