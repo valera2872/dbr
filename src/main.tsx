73910f03ff11498b58319a6d68417f6c90c66a9a
valera2872 <barbarov28@protonmail.com>
2026-08-03 17:16:20 +0300

    fix: load v0.5.1 version guard last

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import './premiumEnhancements';
 import './cameraMeaning';
 import './cameraFrames';
 import './act2HiddenRouteV2';
+import './versionGuard';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
