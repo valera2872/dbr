7e693df629b12b09602e2f82081939122ce0dffd
valera2872 <barbarov28@protonmail.com>
2026-08-03 19:44:16 +0300

    chore: повысить версию до v0.6.2

--- src/build.ts
+++ src/build.ts
@@ -1,6 +1,7 @@
-export const APP_BUILD = 'v0.6.1';
+export const APP_BUILD = 'v0.6.2';
 export const CASE_ID = 'dbr_001_room_314';
 export const CORE_STORAGE_KEY = `dbr:${CASE_ID}:0.2.0`;
 export const ACT2_STORAGE_KEY = `dbr:${CASE_ID}:act2:v0.5.0`;
 export const ACT3_STORAGE_KEY = `dbr:${CASE_ID}:act3:v0.6.0`;
+export const INTERROGATION_STORAGE_KEY = `dbr:${CASE_ID}:interrogation:kirill:v0.6.2`;
 export const PREMIUM_STORAGE_PREFIX = `dbr:${CASE_ID}:premium:`;
